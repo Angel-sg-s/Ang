45a313011871e151999443c2271dc0f2887c45c8
Angel-sg-s <114315557+Angel-sg-s@users.noreply.github.com>
2022-12-15 14:44:43 -0500

    Add files via upload

---
+++ js/Login.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('boton')
+
+document.addEventListener('DOMContentLoaded', function(event){
+    btn.addEventListener('click', function(){
+        boton();
+    })
+})
+function boton(){
+    alert("Todavia no funcina pendejo")
+}
