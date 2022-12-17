05d944b90cea58138844c4046e3e7c8c499e7309
Angel-sg-s <114315557+Angel-sg-s@users.noreply.github.com>
2022-12-16 22:33:10 -0500

    Update Login.js

--- js/Login.js
+++ js/Login.js
@@ -1,13 +1,21 @@
-let btn = document.getElementById('boton')
+let btn = document.getElementById('login')
+let btnd = document.getElementById('continue')
 const switchers = [...document.querySelectorAll('.switcher')]
 
 document.addEventListener('DOMContentLoaded', function(event){
     btn.addEventListener('click', function(){
         boton();
     })
+     btnd.addEventListener('click', function(){
+        botond();
+    })
+})
 })
 function boton(){
-    alert("Todavia no funciona")
+    alert("Todavia no funciona pendejo")
+}
+function botond(){
+    alert("Todavia no funciona pendejo")
 }
 
 
